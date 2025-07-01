624759f98a3df071f829f22099a6f0f58879bafc
Vamsi Sollem <50356552+vamsisollem@users.noreply.github.com>
2025-07-01 11:52:57 -0400

    secured

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4UFSaVWVNQZiXpT0K_tQXuxSqb-uj730",
+  apiKey: "",
   authDomain: "management-app-e142b.firebaseapp.com",
   projectId: "management-app-e142b",
   storageBucket: "management-app-e142b.firebasestorage.app",
   messagingSenderId: "854521776313",
-  appId: "1:854521776313:web:9d4be081ef4dd7eeeb7368",
-  measurementId: "G-TBHJ7T2F5F"
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
